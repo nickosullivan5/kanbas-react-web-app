6f7af358b86ffbd253abe200d025cc8cc773b849
nos <osullivan.nick@icloud.com>
2025-01-15 06:02:32 -0800

    1.3.7

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -2,7 +2,7 @@ import CourseNavigation from "./Navigation.tsx";
 import { Navigate, Route, Routes } from "react-router";
 import Modules from "./Modules";
 import Home from "./Home";
-
+import Assignments from "./Assignments";
 export default function Courses() {
   return (
       <div id="wd-courses">
@@ -18,7 +18,7 @@ export default function Courses() {
                           <Route path="/" element={<Navigate to="Home"/>}/>
                             <Route path="Home" element={<Home />} />
                           <Route path="Modules" element={<Modules />} />
-                          <Route path="Assignments" element={<h2>Assignments</h2>}/>
+                          <Route path="Assignments" element={<Assignments />}/>
                           <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>}/>
                           <Route path="People" element={<h2>People</h2>}/>
                       </Routes>
