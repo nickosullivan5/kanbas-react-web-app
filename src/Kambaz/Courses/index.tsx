bf5edd9c9c8dcef6354b77802ff770c8e3960cc1
nos <osullivan.nick@icloud.com>
2025-01-15 05:01:54 -0800

    Everything up to 1.3.6

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,6 +1,7 @@
 import CourseNavigation from "./Navigation.tsx";
 import { Navigate, Route, Routes } from "react-router";
 import Modules from "./Modules";
+import Home from "./Home";
 
 export default function Courses() {
   return (
@@ -15,7 +16,7 @@ export default function Courses() {
                   <td valign="top">
                       <Routes>
                           <Route path="/" element={<Navigate to="Home"/>}/>
-                          <Route path="Home" element={<h2>Home</h2>}/>
+                            <Route path="Home" element={<Home />} />
                           <Route path="Modules" element={<Modules />} />
                           <Route path="Assignments" element={<h2>Assignments</h2>}/>
                           <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>}/>
