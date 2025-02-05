3f1a61260121e339cd9c98ba5edd90d8879d7492
nos <osullivan.nick@icloud.com>
2025-02-05 09:20:54 -0500

    finished A2

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -1,18 +1,44 @@
 import { Link } from "react-router-dom";
+import {Form} from "react-bootstrap";
 export default function Profile() {
   return (
-    <div id="wd-profile-screen">
-      <h3>Profile</h3>
-      <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
-      <input defaultValue="123"   placeholder="password" type="password" className="wd-password" /><br/>
-      <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
-      <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
-      <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
-      <input defaultValue="alice@wonderland" type="email" id="wd-email" /><br/>
-      <select defaultValue="FACULTY" id="wd-role">
-        <option value="USER">User</option>       <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option> <option value="STUDENT">Student</option>
-      </select><br/>
-      <Link to="/Kambaz/Account/Signin" >Sign out</Link>
+      <div id="wd-profile-screen" className="container">
+          <h1 className={"mb-3"}>Profile</h1>
+          <Form.Control id="wd-username"
+                        defaultValue="alice"
+                        placeholder="username"
+                        className="mb-1"/>
+          <Form.Control id="wd-password"
+                        defaultValue="123"
+                        placeholder="password" type="password"
+                        className="mb-1"/>
+          <Form.Control id="wd-firstname"
+                        defaultValue="Alice"
+                        placeholder="First Name"
+                        className="mb-1"/>
+          <Form.Control id="wd-lastname"
+                        defaultValue="Wonderland"
+                        placeholder="Last Name"
+                        className="mb-1"/>
+          <Form.Control id="wd-wd-dob"
+                        defaultValue="mm/dd/yyyy"
+                        className="mb-1"
+                         type="date"/>
+          <Form.Control id="wd-email"
+                        defaultValue="alice@wonderland"
+                        type="email"
+                        className="mb-1"/>
+          <Form.Select id="wd-role" defaultValue="USER" className="mb-3">
+                        <option value="USER">USER</option>
+                        <option value="ADMIN">ADMIN</option>
+                        <option value="FACULTY">FACULTY</option>
+                        <option value="STUDENT">STUDENT</option>
+        </Form.Select>
+
+          <Link id="wd-signup-btn"
+                to="/Kambaz/Account/Signin"
+                className="btn btn-danger w-100  mb-2">
+              Sign out </Link>
+
     </div>
 );}
--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -6,22 +6,19 @@ import Signup from "./Signup.tsx";
 import AccountNavigation from "./Navigation.tsx";
 export default function Account() {
   return (
-    <div id="wd-account-screen">
-      <table>
-        <tr>
-          <td valign="top">
-            <AccountNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/"        element={<Navigate to="/Kambaz/Account/Signin" />} />
-              <Route path="/Signin"  element={<Signin />} />
-              <Route path="/Profile" element={<Profile />} />
-              <Route path="/Signup"  element={<Signup />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
-    </div>
-
-);}
+      <div id="wd-account-screen" className="ps-4e">
+        <div className="d-flex">
+          <div className="d-none d-md-block">
+            <AccountNavigation/>
+          </div>
+          <div className={"w-50"}>
+          <Routes>
+            <Route path="/" element={<Navigate to="/Kambaz/Account/Signin"/>}/>
+            <Route path="/Signin" element={<Signin/>}/>
+            <Route path="/Profile" element={<Profile/>}/>
+            <Route path="/Signup" element={<Signup/>}/>
+          </Routes>
+          </div>
+        </div>
+      </div>
+        );}
