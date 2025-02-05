1cc728090f89941f7b94e449d1a3ae4e875e0e97
nos <osullivan.nick@icloud.com>
2025-02-05 08:57:03 -0500

    up to 2.4.8

--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -1,126 +1,118 @@
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+
 export default function AssignmentEditor() {
     return (
-        <div id="wd-assignments-editor" className={"container ps-5 pe-5" }>
-                <div className="">
-                    <label htmlFor="wd-name">Assignment Name</label>
-                    <div><input id="wd-name" className="w-100 pt-2" value="A1 - ENV + HTML"/>
+        <Container id="wd-assignments-editor" className="ps-5 pe-5">
+            {/* Assignment Name */}
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="wd-name">Assignment Name</Form.Label>
+                <Form.Control id="wd-name" type="text" defaultValue="A1 " />
+            </Form.Group>
+
+            {/* Description */}
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="wd-description">Description</Form.Label>
+                <Form.Control as="textarea" id="wd-description" style={{ height: "250px" }} defaultValue="The assignment is available online. Submit a link to the landing page of Netlify" />
+            </Form.Group>
+
+            <Row className="mb-3">
+                <Col md={3}>
+                    <Form.Label htmlFor="wd-points">Points</Form.Label>
+                </Col>
+                <Col md={9}>
+                    <Form.Control id="wd-points" type="number" defaultValue={100} />
+                </Col>
+            </Row>
+
+            {/* Assignment Group */}
+            <Row className="mb-3">
+                <Col md={3}>
+                    <Form.Label htmlFor="wd-group">Assignment Group</Form.Label>
+                </Col>
+                <Col md={9}>
+                    <Form.Select id="wd-group">
+                        <option value="ASSIGNMENTS">ASSIGNMENTS</option>
+                        <option value="PROJECTS">PROJECTS</option>
+                        <option value="QUIZZES">QUIZZES</option>
+                    </Form.Select>
+                </Col>
+            </Row>
+
+            {/* Display Grade As */}
+            <Row className="mb-3">
+                <Col md={3}>
+                    <Form.Label htmlFor="wd-display-grade-as">Display Grade as</Form.Label>
+                </Col>
+                <Col md={9}>
+                    <Form.Select id="wd-display-grade-as">
+                        <option value="Percentage">Percentage</option>
+                        <option value="Letter">Letter</option>
+                        <option value="GPA">GPA</option>
+                    </Form.Select>
+                </Col>
+            </Row>
+
+            {/* Submission Type */}
+            <Row className="mb-3">
+                <Col md={3}>
+                    <Form.Label htmlFor="wd-submission-type">Submission Type</Form.Label>
+                </Col>
+                <Col md={9}>
+                    <div className="p-3 border border-secondary-subtle rounded">
+                        <Form.Select id="wd-submission-type">
+                            <option value="Online">Online</option>
+                            <option value="Physical">Physical</option>
+                        </Form.Select>
+
+                        <Form.Label className="mt-2 fw-bold pb-1">Online Entry Options</Form.Label>
+                        <Form.Check className="pb-2" type="checkbox" id="wd-text-entry" label="Text Entry" />
+                        <Form.Check className="pb-2" type="checkbox" id="wd-website-url" label="Website URL" />
+                        <Form.Check className="pb-21" type="checkbox" id="wd-media-recordings" label="Media Recordings" />
+                        <Form.Check className="pb-2" type="checkbox" id="wd-student-annotation" label="Student Annotation" />
+                        <Form.Check className="pb-2" type="checkbox" id="wd-file-upload" label="File Uploads" />
                     </div>
-                </div>
+                </Col>
+            </Row>
 
-                <div className="pt-3">
-                    <div>
-                    <textarea id="wd-description" className="w-100" style={{ height: "250px" }}>
-                        The assignment is available online. Submit a link to the landing page of Netlify
-                    </textarea>
+            <Row className="mb-3">
+                <Col md={3}>
+                    <Form.Label>Assign</Form.Label>
+                </Col>
+                <Col md={9}>
+                    <div className="p-3 border border-secondary-subtle rounded">
+                    <Form.Group>
+                        <Form.Label className="fw-bold" htmlFor="wd-assign-to">Assign To</Form.Label>
+                        <Form.Control id="wd-assign-to" type="text" defaultValue="Everyone" />
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label className="fw-bold" htmlFor="wd-due-date">Due Date</Form.Label>
+                        <Form.Control id="wd-due-date" type="date" defaultValue="2024-05-21" />
+                    </Form.Group>
+
+                    <Row className="mt-2">
+                        <Col>
+                            <Form.Group>
+                                <Form.Label className="fw-bold" htmlFor="wd-available-from">Available From</Form.Label>
+                                <Form.Control id="wd-available-from" type="date" defaultValue="2024-05-14" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label className="fw-bold" htmlFor="wd-available-until">Until</Form.Label>
+                                <Form.Control id="wd-available-until" type="date" defaultValue="2024-05-21" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
                     </div>
-                </div>
+                </Col>
+            </Row>
 
-            <div className="d-flex justify-content-end mb-3 pt-3">
-                <label htmlFor="wd-points"className="pe-2">Points</label>
-                <input id="wd-points" value={100} className="w-75"/>
-            </div>
-
-            <div className="d-flex justify-content-end mb-3">
-                <label htmlFor="wd-group" className="pe-2">Assignment Group</label>
-                <select id="wd-group" className="w-75">
-                    <option value="ASSIGNMENTS">ASSIGNMENTS</option>
-                    <option value="PROJECTS">PROJECTS</option>
-                    <option value="QUIZZES">QUIZZES</option>
-                </select>
-            </div>
-
-            <div className="d-flex justify-content-end mb-3">
-                <label htmlFor="wd-display-grade-as" className="pe-2">Display Grade as</label>
-                <select id="wd-display-grade-as" className="w-75">
-                    <option value="Percentage">Percentage</option>
-                    <option value="Letter">Letter</option>
-                    <option value="GPA">GPA</option>
-                </select>
-            </div>
-
-            <div className="d-flex justify-content-end mb-3">
-                <label htmlFor="wd-submission-type" className="pe-2">Submission Type</label>
-
-                <div className="w-75">
-                    <select id="wd-submission-type" className="w-100">
-                        <option value="Online">Online</option>
-                        <option value="Physical">Physical</option>
-                    </select>
-
-                        <label>Online Entry Options:</label><br/>
-
-                        <input type="checkbox" name="online-entry-options" id="wd-text-entry"/>
-                        <label htmlFor="wd-text-entry">Text Entry</label><br/>
-
-                        <input type="checkbox" name="online-entry-options" id="wd-website-url"/>
-                        <label htmlFor="wd-website-url">Website URL</label><br/>
-
-                        <input type="checkbox" name="conline-entry-options" id="wd-media-recordings"/>
-                        <label htmlFor="wd-media-recordings">Media Recordings</label><br/>
-
-                        <input type="checkbox" name="online-entry-options" id="wd-student-annotation"/>
-                        <label htmlFor="wd-student-annotation">Student Annotation</label><br/>
-
-                        <input type="checkbox" name="online-entry-options" id="wd-file-upload"/>
-                        <label htmlFor="wd-file-upload">File Uploads</label>
-                </div>
-            </div>
-            <div className="d-flex justify-content-end mb-3">
-                <text className="pe-2">Assign</text>
-                <div className="w-75">
-                    <div>
-                        <label htmlFor="wd-assign-to" className="pe-2">Assign To</label>
-                    </div>
-                    <div>
-                        <input id="wd-assign-to" className="w-75" value={"Everyone"}/>
-                    </div>
-                    <div>
-                        <label htmlFor="wd-due-date" className="pe-2">Due Date</label>
-                    </div>
-                    <div>
-                        <input type="date" value="2024-05-21" id="wd-due-date" className="w-75"/>
-                    </div>
-                    <div className="d-inline-flex justify-content-between">
-                        <div >
-                            <div>
-                                <label htmlFor="wd-available-from" className="pe-2">Available From</label>
-
-                            </div>
-                            <input type="date" value="2024-05-14" id="wd-available-from" className="w-75"/>
-                        </div>
-
-                        <div>
-                            <div>
-                                <label htmlFor="wd-available-until" className="pe-2">Until</label>
-
-                            </div>
-                            <input type="date" value="2024-05-21" id="wd-available-until" className="w-75"/>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
-            <div className="d-flex justify-content-end mb-3">
-
-            </div>
-
-            <div className="d-flex justify-content-end mb-3">
-
-            </div>
-
-            <div className="d-flex justify-content-end mb-3">
-
-            </div>
-
-            <div className="d-flex justify-content-end mb-3">
-
-            </div>
-
+            {/* Buttons */}
             <div className="d-flex justify-content-end mt-3">
-                <button className="rounded-0 me-2" style={{border: '1px solid #d3d3d3'}}>Cancel</button>
-                <button className="rounded-0 border-0 bg-danger text-white">Save</button>
+                <Button variant="light" className="me-2 border">Cancel</Button>
+                <Button variant="danger">Save</Button>
             </div>
-        </div>
+        </Container>
     );
 }
