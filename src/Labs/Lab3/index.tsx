c1ed073667c2ec8b020802d679b3825779dfeeb7
nos <osullivan.nick@icloud.com>
2025-02-19 09:07:51 -0500

    3.3.1

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,6 +1,31 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes.tsx";
+import BooleanVariables from "./BooleanVariables.tsx"
+import IfElse from "./IfElse.tsx";
+import TernaryOperator from "./TernaryOperator.tsx";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse.tsx";
+import ConditionalOutputInline from "./ConditionalOutputInline.tsx";
+import LegacyFunctions from "./LegacyFunctions.tsx";
+import ArrowFunctions from "./ArrowFunctions.tsx";
+import ImpliedReturn from "./ImpliedReturn.tsx";
+import TemplateLiterals from "./TemplateLiterals.tsx";
+import SimpleArrays from "./SimpleArrays.tsx";
 export default function Lab3() {
-  return (
-    <div>
-      <h2>Lab 3</h2>
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+        <VariableTypes/>
+        <BooleanVariables/>
+        <IfElse/>
+        <TernaryOperator/>
+        <ConditionalOutputIfElse/>
+        <ConditionalOutputInline/>
+        <LegacyFunctions/>
+        <ArrowFunctions/>
+        <ImpliedReturn/>
+        <TemplateLiterals/>
+      <SimpleArrays/>
     </div>
-);}
+  );
+}
