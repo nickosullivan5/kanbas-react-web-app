490624bff69fdf373945025660b2efed5b7c3744
nos <osullivan.nick@icloud.com>
2025-04-02 18:11:11 -0400

    onto 5.2.4.4

---
+++ src/Labs/Lab5/index.tsx
@@ -0,0 +1,23 @@
+import EnvironmentVariables from "./EnvironmentVariables";
+// import PathParameters from "./PathParameters.tsx";
+import QueryParameters from "./ QueryParameters.tsx";
+import WorkingWithObjects from "./WorkingWithObjects.tsx";
+import WorkingWithArrays from "../../Kambaz/Database/WorkingWithArrays.tsx";
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+export default function Lab5() {
+  return (
+    <div id="wd-lab5">
+      <h2>Lab 5</h2>
+      <div className="list-group">
+        <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
+          Welcome
+        </a>
+      </div><hr />
+      {/*<EnvironmentVariables />*/}
+      <EnvironmentVariables />
+      {/*<PathParameters />*/}
+      <QueryParameters />
+        <WorkingWithObjects/>
+        <WorkingWithArrays/>
+    </div>
+);}
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,6 +4,7 @@ import Lab3 from "./Lab3";
 import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC.tsx";
 import Lab4 from "./Lab4";
+import Lab5 from "./Lab5";
 import store from "./store";
 import { Provider } from "react-redux";
 export default function Labs() {
@@ -19,6 +20,8 @@ export default function Labs() {
         <Route path="Lab2/*" element={<Lab2 />} />
         <Route path="Lab3/*" element={<Lab3 />} />
           <Route path="Lab4/*" element={<Lab4 />} />
+                  <Route path="Lab5/*" element={<Lab5 />} />
+
       </Routes>
     </div>
                   </Provider>
