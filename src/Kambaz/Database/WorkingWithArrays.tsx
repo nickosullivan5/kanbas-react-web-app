a0ec14d4092bbd644e2b01d4976461f37048e743
nos <osullivan.nick@icloud.com>
2025-04-02 18:31:29 -0400

    onto 5.2.4.7

--- src/Kambaz/Database/WorkingWithArrays.tsx
+++ src/Kambaz/Database/WorkingWithArrays.tsx
@@ -3,7 +3,11 @@ import {useState} from "react";
 
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export default function WorkingWithArrays() {
-        const [todo, setTodo] = useState({id: "1"});
+        const [todo, setTodo] = useState({id: "1",
+            title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-09-09",
+    completed: false,});
 
     const API = `${REMOTE_SERVER}/lab5/todos`;
   return (
@@ -26,6 +30,27 @@ export default function WorkingWithArrays() {
              href={`${API}?completed=true`}>
               Get Completed Todos
           </a>
+          <hr/>
+          <h3>Creating new Items in an Array</h3>
+          <a id="wd-retrieve-completed-todos" className="btn btn-primary"
+             href={`${API}/create`}>
+              Create Todo
+          </a>
+          <hr/>
+          <h3>Deleting from an Array</h3>
+          <a id="wd-retrieve-completed-todos" className="btn btn-primary float-end" href={`${API}/${todo.id}/delete`}>
+              Delete Todo with ID = {todo.id} </a>
+          <FormControl defaultValue={todo.id} className="w-50"
+                       onChange={(e) => setTodo({...todo, id: e.target.value})}/>
+          <hr/>
+          <h3>Updating an Item in an Array</h3>
+          <a href={`${API}/${todo.id}/title/${todo.title}`} className="btn btn-primary float-end">
+              Update Todo</a>
+          <FormControl defaultValue={todo.id} className="w-25 float-start me-2"
+                       onChange={(e) => setTodo({...todo, id: e.target.value})}/>
+          <FormControl defaultValue={todo.title} className="w-50 float-start"
+                       onChange={(e) => setTodo({...todo, title: e.target.value})}/>
+          <br/><br/>
           <hr/>
 
 
