89e4f6f577d4bc6d4da65f9966593e508ab1a444
nos <osullivan.nick@icloud.com>
2025-04-02 23:09:03 -0400

    moving onto 5.3.6

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -4,30 +4,52 @@ import {BsGripVertical} from "react-icons/bs";
 import ModuleControlButtons from "./ModuleControlButtons.tsx";
 import LessonControlButtons from "./LessonControlButtons.tsx";
 import {useParams} from "react-router";
-import {useState} from "react";
-import {addModule, editModule, updateModule, deleteModule}
-    from "./reducer";
+
 import {useSelector, useDispatch} from "react-redux";
 import FacultyOnlyRoute from "../../Account/FacultyOnlyRoute.tsx";
+import {setModules, addModule, editModule, updateModule, deleteModule} from "./reducer";
+import {useState, useEffect} from "react";
+import * as coursesClient from "../client";
+import * as modulesClient from "./client";
 
 export default function Modules() {
     const {cid} = useParams();
     const [moduleName, setModuleName] = useState("");
     const {modules} = useSelector((state: any) => state.modulesReducer);
     const dispatch = useDispatch();
+    const fetchModules = async () => {
+        const modules = await coursesClient.findModulesForCourse(cid as string);
+        dispatch(setModules(modules));
+    };
+    const createModuleForCourse = async () => {
+        if (!cid) return;
+        const newModule = {name: moduleName, course: cid};
+        const module = await coursesClient.createModuleForCourse(cid, newModule);
+        dispatch(addModule(module));
+    };
+    const removeModule = async (moduleId: string) => {
+    await modulesClient.deleteModule(moduleId);
+    dispatch(deleteModule(moduleId));
+  };
+  const saveModule = async (module: any) => {
+    await modulesClient.updateModule(module);
+    dispatch(updateModule(module));
+  };
+
+
+    useEffect(() => {
+        fetchModules();
+    }, []);
 
 
     return (
 
-        <Container className="position-relative"> {/* Position context for absolute elements */}
-            {/* Make the module controls stack over the list */}
+        <Container className="position-relative">
             <FacultyOnlyRoute>
 
                 <div className="pt-3 position-sticky top-0 bg-white z-3">
-                    <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={() => {
-                        dispatch(addModule({name: moduleName, course: cid}));
-                        setModuleName("");
-                    }}/>
+                    <ModulesControls setModuleName={setModuleName} moduleName={moduleName}
+                                     addModule={createModuleForCourse}/>
 
                 </div>
             </FacultyOnlyRoute>
@@ -36,7 +58,7 @@ export default function Modules() {
             <div className="z-2 mt-5"> {/* Added margin-top to separate the list from the controls */}
                 <ListGroup id="wd-modules" className="rounded-0">
                     {modules
-                        .filter((module: any) => module.course === cid)
+
                         .map((module: any) => (
                             <ListGroup.Item key={module._id} className="wd-module p-0 mb-5 fs-5 border-gray">
                                 <div className="wd-title p-3 ps-2 bg-light">
@@ -50,21 +72,20 @@ export default function Modules() {
                                                      }
                                                      onKeyDown={(e) => {
                                                          if (e.key === "Enter") {
-                                                             dispatch(updateModule({...module, editing: false}));
+                                                               saveModule({ ...module, editing: false });
+
                                                          }
                                                      }}
                                                      defaultValue={module.name}/>
                                     )}
-                                              <FacultyOnlyRoute>
+                                    <FacultyOnlyRoute>
 
-                                    <ModuleControlButtons
-                                        moduleId={module._id}
-                                        deleteModule={(moduleId) => {
-                                            dispatch(deleteModule(moduleId));
-                                        }}
-                                        editModule={(moduleId) => dispatch(editModule(moduleId))}/>
-                                                            </FacultyOnlyRoute>
+                                        <ModuleControlButtons
+                                            moduleId={module._id}
+                                           deleteModule={(moduleId) => removeModule(moduleId)}
 
+                                            editModule={(moduleId) => dispatch(editModule(moduleId))}/>
+                                    </FacultyOnlyRoute>
 
 
                                 </div>
--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -14,3 +14,17 @@ export const updateCourse = async (course: any) => {
   return data;
 };
 
+export const findModulesForCourse = async (courseId: string) => {
+  const response = await axios
+    .get(`${COURSES_API}/${courseId}/modules`);
+  return response.data;
+};
+
+export const createModuleForCourse = async (courseId: string, module: any) => {
+  const response = await axios.post(
+    `${COURSES_API}/${courseId}/modules`,
+    module
+  );
+  return response.data;
+};
+
